c0bd55a40307c4affe4c9c9c688c9935d9dead2d
monkey <monkey@BB21009>
2016-11-28 14:43:44 -0600

    Add project files.

---
+++ UMLDemo/Profession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UMLDemo
+{
+    public abstract class Profession : Professor
+    {
+        public void GoToWork()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
